dd23264a156686532d4beb1b2d4887f292b5a056
Kristin Dickerson <kdickerson97@gmail.com>
2025-03-31 13:51:21 -0700

    Ignore .DS_Store files

---
+++ make_mesh/dfnworks-output/ready/zoned/100_100_50_0fr_sm5/run_workflow_NoFrac.sh
@@ -0,0 +1,4 @@
+python create_yaml.py
+python middle_layer.py
+python faults.py 
+python combined_driver_v3_NoFrac.py 
---
+++ make_mesh/dfnworks-output/ready/zoned/100_100_50_3fr_smNONE/run_workflow_smNONE.sh
@@ -0,0 +1,4 @@
+python create_yaml.py
+python middle_layer_smNONE.py
+python faults.py 
+python combined_driver_v3.py 
